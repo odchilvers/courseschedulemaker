0a26f9c17582bf087218498345811a91dfc8c94c
Olivia Chilvers <69412454+odchilvers@users.noreply.github.com>
2023-03-17 13:30:35 -0700

    error with making sections appear within classes

--- schedulemaker.js
+++ schedulemaker.js
@@ -14,3 +14,26 @@ window.onload = function () { // makes sure to run when the window has loaded
         }
     });
 }
+
+function courseNameType(){
+    var courseNameType = document.getElementById("courseType").value;
+    alert(courseNameType); // !!!!!!!!!! take out but is working!!!! 
+}
+    
+
+// figure out how to make a class object that takes in name, number of sections (sections to be object that takes in days to meet and times)
+
+// const sectionDiv = document.getElementById("sectionInfoForms"); // gets the classInfoForms data
+// const sectionContainer = document.getElementById("sectionWrapper");
+// const sectionSelect = document.getElementById("numSections"); // gets number of classes input
+// let sectionElementCount = 0;
+
+// window.onload = function () { // makes sure to run when the window has loaded
+//     sectionSelect.addEventListener("change", s => {
+//         sectionElementCount = s.target.value;
+//         sectionContainer.innerHTML = '';
+//         for (var i = 0; i < sectionElementCount; i++) {
+//             sectionContainer.appendChild(sectionDiv.cloneNode(true)); // clones the forms to display
+//         }
+//     });
+// }
