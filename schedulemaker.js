2117cac94583557fc847796f3e015ba13a591bc7
Olivia Chilvers <69412454+odchilvers@users.noreply.github.com>
2023-03-17 14:52:27 -0700

    made updates - class and sections still overlap

--- schedulemaker.js
+++ schedulemaker.js
@@ -1,9 +1,14 @@
 // javascript that goes with the schedulemaker html
 
-const myDiv = document.getElementById("classInfoForms"); // gets the classInfoForms data
-const container = document.getElementById("classWrapper");
-const select = document.getElementById("numClasses"); // gets number of classes input
+const myDiv = document.getElementsByClassName("classInfoForms")[0]; // gets the classInfoForms data
+const container = document.getElementsByClassName("classWrapper")[0];
+const select = document.getElementsByClassName("numClasses")[0]; // gets number of classes input
 let elementCount = 0;
+
+const sectionDiv = myDiv.getElementsByClassName("sectionInfoForms")[0]; // gets the sectionInfoForms data
+const sectionContainer = myDiv.getElementsByClassName("sectionWrapper")[0];
+const sectionSelect = myDiv.getElementsByClassName("numSections")[0]; // gets number of sections input
+let sectionElementCount = 0;
 
 window.onload = function () { // makes sure to run when the window has loaded
     select.addEventListener("change", e => {
@@ -13,27 +18,18 @@ window.onload = function () { // makes sure to run when the window has loaded
             container.appendChild(myDiv.cloneNode(true)); // clones the forms to display
         }
     });
+    sectionSelect.addEventListener("change", s => {
+        sectionElementCount = s.target.value;
+        sectionContainer.innerHTML = '';
+        for (var i = 0; i < sectionElementCount; i++) {
+            sectionContainer.appendChild(sectionDiv.cloneNode(true)); // clones the forms to display
+        }
+    });
 }
 
-function courseNameType(){
-    var courseNameType = document.getElementById("courseType").value;
+function courseNameType() {
+    var courseNameType = document.getElementByClassName("courseType").value;
     alert(courseNameType); // !!!!!!!!!! take out but is working!!!! 
 }
-    
 
 // figure out how to make a class object that takes in name, number of sections (sections to be object that takes in days to meet and times)
-
-// const sectionDiv = document.getElementById("sectionInfoForms"); // gets the classInfoForms data
-// const sectionContainer = document.getElementById("sectionWrapper");
-// const sectionSelect = document.getElementById("numSections"); // gets number of classes input
-// let sectionElementCount = 0;
-
-// window.onload = function () { // makes sure to run when the window has loaded
-//     sectionSelect.addEventListener("change", s => {
-//         sectionElementCount = s.target.value;
-//         sectionContainer.innerHTML = '';
-//         for (var i = 0; i < sectionElementCount; i++) {
-//             sectionContainer.appendChild(sectionDiv.cloneNode(true)); // clones the forms to display
-//         }
-//     });
-// }
