f3a55d9db75abc266a92756eb1d51d1274474d5b
Olivia Chilvers <69412454+odchilvers@users.noreply.github.com>
2023-03-19 15:01:59 -0700

    adds a loader to the schedulemaker page

--- schedulemaker.js
+++ schedulemaker.js
@@ -107,3 +107,17 @@ function scrollUp() { // scroll to middle of page where the how to guide is
         behavior: 'smooth'
     });
 }
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+        "body").style.visibility = "hidden";
+        document.querySelector(
+        "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+        "#loader").style.display = "none";
+        document.querySelector(
+        "body").style.visibility = "visible";
+    }
+};
